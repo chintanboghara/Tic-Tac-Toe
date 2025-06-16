7d7ed8d611e8da4b40456ae7380ac706badf9da9
Chintan Boghara <chintansboghara@gmail.com>
2025-06-16 10:37:21 +0530

    refactor: Remove comments from postcss.config.js for improved clarity

--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,6 @@
-/**
- * PostCSS Configuration
- *
- * This configuration enables Tailwind CSS and autoprefixer.
- * - Tailwind CSS: Processes your CSS using Tailwind's utility-first framework.
- * - Autoprefixer: Automatically adds vendor prefixes for cross-browser compatibility.
- */
 export default {
   plugins: {
-    tailwindcss: {}, // Process Tailwind CSS classes
-    autoprefixer: {}, // Add vendor prefixes to CSS rules
+    tailwindcss: {},
+    autoprefixer: {},
   },
 };
