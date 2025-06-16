9fcc3d2788f5970202bbe28def3bcac56f8ead9d
Chintan Boghara <chintansboghara@gmail.com>
2025-06-16 10:37:37 +0530

    refactor: Simplify main.tsx by removing unused providers and comments

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,10 @@
-import React, { StrictMode } from 'react'; // Import React
+import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import { ThemeProvider } from './contexts/ThemeContext'; // Import ThemeProvider
-import { SoundProvider } from './contexts/SoundContext'; // Import SoundProvider
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ThemeProvider>
-      <SoundProvider>
-        <App />
-      </SoundProvider>
-    </ThemeProvider>
+    <App />
   </StrictMode>
 );
